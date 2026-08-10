94126e16fdbca41116123091c30e5bfb6eb28ade
Your Name <wubetumasud@gmail.com>
2026-08-10 17:43:20 +0300

    declare variable in go with var

---
+++ 02_variables_and_types/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var channelName string;
+	channelName = "MasudDev"
+
+	var year int = 2026
+
+	fmt.Println("Channel: ", channelName)
+	fmt.Println("Year: ", year)
+
+	var rating float64 = 4.8
+	fmt.Println("Rating: ", rating)
+}
