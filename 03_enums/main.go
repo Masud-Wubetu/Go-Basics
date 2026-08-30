b6fc35194864db44605e03ff3dfcfd0246cd357f
Your Name <wubetumasud@gmail.com>
2026-08-31 02:00:49 +0300

    switch with multiple case values

--- 03_enums/main.go
+++ 03_enums/main.go
@@ -1,13 +1,11 @@
 package main
 import "fmt"
 func main() {
-	day := 4
-	switch {
-	case day == 1:
-		fmt.Println("Monday")
-	case day == 4:
-		fmt.Println("Thursday")
-	case day > 5:
+	day := 6
+	switch day {
+	case 1, 2, 3, 4, 5:
+		fmt.Println("Weekday")
+	case 6, 7:
 		fmt.Println("Weekend")
 	default:
 		fmt.Println("Invalid day")
