870c25bf4ec9a04b3c3bab955d49ef090e218a45
Your Name <wubetumasud@gmail.com>
2026-08-31 11:11:22 +0300

    funxtion in go

--- 03_enums/main.go
+++ 03_enums/main.go
@@ -1,15 +1,9 @@
 package main
 import "fmt"
+func multiply(a, b int) int {
+	return a * b
+}
 func main() {
-	var value interface{} = 3.14
-	switch v := value.(type) {
-	case int:
-		fmt.Println("Integer:", v)
-	case string:
-		fmt.Println("String:", v)
-	case float64:
-		fmt.Println("Float:", v)
-	default:
-		fmt.Printf("Unknown type: %T\n", v)
-	}
+	result := multiply(5, 10)
+	fmt.Println(result)
 }
